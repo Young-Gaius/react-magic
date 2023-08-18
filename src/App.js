01237ef736b7147bc9baa42937186b006ed95255
Young-Gaius <youngehu8@gmail.com>
2023-08-18 18:12:40 +0100

    add quote file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <h1>Calculator App</h1>
       <Calculator />
+      <Quote />
     </div>
   );
 }
