2c1498aeadb0837325b51fcd47db75204d4deb88
Young-Gaius <youngehu8@gmail.com>
2023-08-24 20:56:55 +0100

    created test file

---
+++ src/components/Test/Navbar.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Navbar from '../Navbar';
+
+test('Navbar renders correctly', () => {
+  const component = renderer.create(
+    <Router>
+      <Navbar />
+    </Router>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
